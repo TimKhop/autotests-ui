00bf95abc0c80a4f20a51bac9cb0338c5030a7de
TimKhop <timkhop2003@gmail.com>
2025-11-19 08:43:21 +0400

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,8 +1,33 @@
 from playwright.sync_api import sync_playwright, expect
 
+# with sync_playwright() as playwright:
+    # browser = playwright.chromium.launch(headless=False)
+    # page = browser.new_page()
+    #
+    # page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    #
+    # email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    # email_input.fill('user.name@gmail.com')
+    #
+    # username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    # username_input.fill('username')
+    #
+    # password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    # password_input.fill('password')
+    #
+    # registration_button = page.get_by_test_id('registration-page-registration-button')
+    # registration_button.click()
+    #
+    # dashboard_name = page.get_by_test_id('dashboard-toolbar-title-text')
+    # expect(dashboard_name).to_be_visible()
+    # expect(dashboard_name).to_have_text('Dashboard')
+    #
+    # page.wait_for_timeout(5000)
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -18,10 +43,17 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    dashboard_name = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_name).to_be_visible()
-    expect(dashboard_name).to_have_text('Dashboard')
+    context.storage_state(path='browser-state.json')
 
-    page.wait_for_timeout(5000)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(50000)
+
+
 
 
