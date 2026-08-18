a5531c2b1a555573f76546dec5bdfdf35e4ba080
TimKhop <timkhop2003@gmail.com>
2026-08-18 07:06:08 +0400

    8.3 Добавление Allure шагов в автотесты

--- components/courses/create_course_exercise_form_component.py
+++ components/courses/create_course_exercise_form_component.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 
 from elements.input import Input
 from elements.text import Text
-
+import allure
 from elements.button import Button
 
 
@@ -27,6 +27,7 @@ class CreateCourseExerciseFormComponent(BaseComponent):
     def click_delete_button(self, index: int):
         self.delete_exercise_button.click(index=index)
 
+    @allure.step("Check visible create course exercise form at '{index}'")
     def check_visible(self, index: int, title: str, description: str):
 
         self.subtitle.check_visible(index=index)
@@ -38,6 +39,7 @@ class CreateCourseExerciseFormComponent(BaseComponent):
         self.description_input.check_visible(index=index)
         self.description_input.check_have_value(description, index=index)
 
+    @allure.step("Fill create course exercise form at '{index}'")
     def fill_create_exercise_form(self, index: int, title: str, description: str):
 
         self.title_input.fill(title, index=index)
